dfb788c6f0ff35a7b1f858ee934613d087c3ac2d
neizvestniy <mirzamidinovziyobek@gmail.com>
2025-06-18 13:37:04 +0500

    finish the task

--- homework10.py
+++ homework10.py
@@ -1,42 +1,6 @@
-# 1- misol
-# m=int(input("raqam kiriting"))
-# if m % 2 == 0:
-#     print(f"{m} bu juft son")
-# else:
-#     print(f"{m}bu tog son")
-
-# 2-misol
-# m=int(input("raqam kiriting"))
-# if m > 0:
-#     print(f"{m} bu musbat son")
-#
-# elif m == 0:
-#     print(f"{m} bu nol" )
-#
-# else:
-#     print(f"{m} bu manfiy")
-# 3-misol
-
-# m = []
-# n = int(input("Raqam kiriting: "))
-# s= int(input("Raqam kiriting: "))
-# w= int(input("Raqam kiriting: "))
-# a= int(input("Raqam kiriting: "))
-# x= int(input("Raqam kiriting: "))
-# l= int(input("Raqam kiriting: "))
-# m.append(n)
-# m.append(s)
-# m.append(w)
-# m.append(a)
-# m.append(x)
-# m.append(l)
-# print(max(m))
 
 
-# print(max(m))
 
-# m=[1,4,]
-# 5- misol
 m=input("parolni  kiriting")
 if m == "secret":
     print(f"{m} xush kelibsiz")
