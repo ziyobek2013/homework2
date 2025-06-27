e81d6c3c102768dfd3d888c8ecef6122126c3189
neizvestniy <mirzamidinovziyobek@gmail.com>
2025-06-27 16:24:53 +0500

    finish the task

--- dd.py
+++ dd.py
@@ -1,11 +1,6 @@
-# 1- misol
-# ismlar = []
-# while 0 < 5:
-#         ism = input("Ismingizni kiriting: ")
-#         print(f"salom,{ism}")
-# 2- misol
-for son in range(11):  
-    if son % 2 == 0:
-        print(f"{son} juft son")
-    else:
-        print(f"{son} toq son")
+
+ismlar = []
+while 0 < 5:
+        ism = input("Ismingizni kiriting: ")
+
+
---
+++ zz.py
@@ -0,0 +1,22 @@
+
+# summa =0
+# while True:
+#     jammi_summa = int(input("pul kiriting"))
+#     summa += jammi_summa
+#     print(summa)
+#     if jammi_summa >= 10000:
+#         print("Maqsadga erishdingiz sekin ota-onangizni Hajga jonatsangiz bolai")
+#         break
+#
+#     elif summa != 10000:
+#         print("Hali pul chopa vering mablag' yig'ilmadi")
+lst = [1, 2, 3, 4, 5, ]
+for x in lst:
+    print("o'quvchi bahosi ")
+    
+
+
+
+
+
+
