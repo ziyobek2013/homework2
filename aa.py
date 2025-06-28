309b9eb96bc98acdb07e07ba1cdf6d77e0269b24
neizvestniy <mirzamidinovziyobek@gmail.com>
2025-06-28 14:23:23 +0500

    finish the 5 task

--- aa.py
+++ aa.py
@@ -6,10 +6,3 @@
 # kiritilgan_sozlar = ["olma", "non", "ikki", "bob", "salom"]
 # print(palindrom_sozlar(kiritilgan_sozlar))
 
-teskari_soz = input("Soz kiritin:")
-lst = []
-if teskari_soz == teskari_soz[::-1]:
-    lst.append(teskari_soz)
-
-else:
-    print(f"{teskari_soz} bu soz togri kelmaydi")
