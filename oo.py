f23b10c24700687a09aab7034484388b549966b4
neizvestniy <mirzamidinovziyobek@gmail.com>
2025-06-30 18:01:05 +0500

    fini the task

---
+++ oo.py
@@ -0,0 +1,27 @@
+# 1 - misol
+# lugat = {"name": "ziyobek"}
+# if lugat == {}:
+#     print(False)
+# else:
+#     print(True)
+# 2- misol
+# lst = [1,2, "a","b"]
+# m = []
+# for x in lst:
+#      if type(x) == int:
+#          m.append(x)
+#          print(m)
+# 3-misol
+# lst = ["Emy","Adam","Tanya","Emy"]
+# lst2 = []
+# for x in lst:
+#     if x not in lst2:
+#         lst2.append(x)
+# print(lst2)
+# 4 - misol
+# lst = [1,2, "a","b"]
+# m = []
+# for x in lst:
+#     if x == int:
+#         m.append(x)
+#         print(m)
