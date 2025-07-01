1cf66b3531268f19b71138af4eb8ba01feb4af8f
neizvestniy <mirzamidinovziyobek@gmail.com>
2025-07-01 13:33:18 +0500

    finish the task

--- t.py
+++ t.py
@@ -7,3 +7,17 @@
 #             lst[i], lst[j] = lst[j], lst[i]
 #
 # print(lst)
+
+m=input("Parol  kiriting:")
+if m == "2013":
+    print(f"{m} Xush kelibsiz ")
+
+else:
+    print ("Parol notogri: ")
+
+m=input("Karta raqam kiriting:")
+if m == "9860.0101.6912.5645":
+    print(f"{m} Xush kelibsiz Mirzamidinov Ziyobek")
+
+else:
+    print ("Karta raqam notogri: ")
