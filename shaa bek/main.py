737a6aa2c1d34c4c6826b0ab43628977517d3ab8
neizvestniy <mirzamidinovziyobek@gmail.com>
2025-07-01 13:29:20 +0500

    finish the task

---
+++ shaa bek/main.py
@@ -0,0 +1,13 @@
+
+
+# 2-misol
+for son in range(11):
+    if son % 2 == 0:
+        print(f"{son} juft son")
+    else:
+        print(f"{son}tog son")
+
+ismlar = []
+while 0 < 5:
+    ism = input("Ismingizni kiriting: ")
+    print(f"asalomu alekom {ism}")
---
+++ t.py
@@ -0,0 +1,9 @@
+
+# lst = ["a", "ccc", "dddd", "bb"]
+#
+# for i in range(len(lst)):
+#     for j in range(i + 1, len(lst)):
+#         if len(lst[i]) > len(lst[j]):
+#             lst[i], lst[j] = lst[j], lst[i]
+#
+# print(lst)
