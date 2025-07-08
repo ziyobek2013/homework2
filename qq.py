abf1d0e6f36485c3c3ff587c6d3cf4ee26f94dfa
neizvestniy <mirzamidinovziyobek@gmail.com>
2025-07-08 14:00:08 +0500

    finish the task

---
+++ qq.py
@@ -0,0 +1,12 @@
+# def reverse(txt):
+#     return txt[::-1]
+# print(reverse("hello world"))
+# print(reverse("ReVeRse"))
+# 2 - misol
+def teskari_soz(name):
+    first, last = name(" ")
+    return first
+
+print(teskari_soz("Donald Trump"))
+teskari_soz("Rosie O'Donnell")
+teskari_soz("Seymour Butts")
