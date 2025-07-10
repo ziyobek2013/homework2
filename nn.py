b7cb783c4338b7658d2556423b7d5c6f5c90fc0a
neizvestniy <mirzamidinovziyobek@gmail.com>
2025-07-10 15:13:47 +0500

    finish the task

--- nn.py
+++ nn.py
@@ -1,18 +1,26 @@
-# def karta_raqam (rqam):
-#     return rqam
-# print(karta_raqam("************5678"))
-# print(karta_raqam("************3213"))
-# print(karta_raqam("************5523"))
-def bro(word):
-    jami = 0
-    vowels = "aeio"
-    for x in word:
-        for i in vowels:
-            if x == i:
-                jami += 1
-
-    return jami
-
-print(bro("Celebration"))
-print(bro("Palm"))
-print(bro("Prediction"))
+# # def karta_raqam (rqam):
+# #     return rqam
+# # print(karta_raqam("************5678"))
+# # print(karta_raqam("************3213"))
+# # print(karta_raqam("************5523"))
+# def bro(word):
+#     jami = 0
+#     vowels = "aeio"
+#     for x in word:
+#         for i in vowels:
+#             if x == i:
+#                 jami += 1
+#
+#     return jami
+#
+# print(bro("Celebration"))
+# print(bro("Palm"))
+# print(bro("Prediction"))
+def kopaytir(*args):
+    natija = 1
+    for son in args:
+        natija *= son
+    return natija
+print(kopaytir(2, 3, 4))
+print(kopaytir(5))        # Natija: 5
+print(kopaytir())         # Natija: 1
